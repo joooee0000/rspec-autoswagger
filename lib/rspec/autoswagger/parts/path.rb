d0e8aae8d309f616b6b750ce0d1ae97fbd9ddde0
joooee0000 <joooee0000@gmail.com>
2018-06-18 15:45:39 +0900

    fetch parameters from request

--- lib/rspec/autoswagger/parts/path.rb
+++ lib/rspec/autoswagger/parts/path.rb
@@ -16,16 +16,33 @@ module Rspec
           example.full_description[%r<(GET|POST|PATCH|PUT|DELETE) ([^ ]+)>, 2]
         end
 
+        def tags
+          [request.parameters['controller']]
+        end
+
         def status
           response.status
         end
 
         def method
-          request.method
+          request.method.downcase
         end
 
         def params
-          request.parameters
+          params = request.parameters
+          params.delete('controller')
+          params.delete('action')
+          params
+        end
+
+        def generate_parameters
+          params.map do |name, value|
+            {
+              'name' => name,
+              'in' => 'path',
+              'type' => convert_value_to_type(value)
+            }
+          end
         end
 
         def generate_hash
@@ -33,17 +50,32 @@ module Rspec
           hash[path] = {}
           hash[path][method] = {}
           hash[path][method] = {}
-          hash[path][method]["tags"] = []
+          hash[path][method]["tags"] = tags
           hash[path][method]["summary"] = []
           hash[path][method]["description"] = description
-          hash[path][method]["parameters"] = []
-          hash[path][method]["parameters"] << { 'name' => 'hoge', 'in' => 'hoge', 'required' => true, 'type' => 'hoge'}
+
+          hash[path][method]["parameters"] = generate_parameters
+
           hash[path][method]["produces"] = ['application/json']
           hash[path][method]["responses"] = {}
           hash[path][method]["responses"][status] = {}
           hash[path][method]["responses"][status]["description"] = "successful operation"
           hash[path][method]["responses"][status]["schema"] = { "$ref" => "#/definitions/#{response_name}" }
           hash
+        end
+
+        private
+
+        def convert_value_to_type(value)
+          if value.to_s == 'true' || value.to_s == 'false'
+            'boolean'
+          elsif value.is_a?(String)
+            'string'
+          elsif value.is_a?(Integer)
+            'integer'
+          else
+            'string'
+          end
         end
       end
     end
